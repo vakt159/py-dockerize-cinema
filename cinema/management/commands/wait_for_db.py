da2b120bafab952e12a49daf0205feef975d7cad
Andrii Yermolenko <vakt159@gmail.com>
2026-01-08 17:03:36 +0200

    flake8 fix

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for the database to be available"""
 
