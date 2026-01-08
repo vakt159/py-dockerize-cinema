8b3b76b1599bf850e0daa37300a4803223ddaac1
Andrii Yermolenko <vakt159@gmail.com>
2026-01-08 16:59:41 +0200

    Solution

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:3.13-alpine
+
+LABEL maintainer="vakt159@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+RUN mkdir -p /files/media
+
+RUN adduser \
+    --disabled-password \
+    --no-create-home \
+    my_user
+
+RUN chown -R my_user:my_user /files/media
+RUN chmod -R 775 /files/media
+
+USER my_user
---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,21 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for the database to be available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                # Try to get the "default" database connection
+                connections["default"].cursor()
+                db_conn = True
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
